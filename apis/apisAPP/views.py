96b13f02085ffbd01cc5b6c254e1ca7251b820c5
Nachc Borrelli <nacho2911@hotmail.com>
2022-01-11 19:32:37 -0300

    Ahora es por post, no por get

--- apis/apisAPP/views.py
+++ apis/apisAPP/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .tasks import save_data
@@ -7,13 +8,15 @@ from .tasks import save_data
         
 class createDatabase(APIView):
 
-    def get(self, request):
+    def post(self, request):
         save_data()
         content = {'message': 'The database has been created'}
         return Response(content)
 
+    def get(self, request):
+        return HttpResponseNotFound("You must use POST in order to trigger this function")
+
 class searchByKeyword(APIView):
 
     def get(self, request):
-        content = {'message': 'The database has been created'}
-        return Response(content)
+        return HttpResponseNotFound("You must use POST in order to get the data")
